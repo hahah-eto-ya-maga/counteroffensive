6b1e4494447a284682abef48d1bb2208eaa1ee4b
Shi-Geo <shihaleev.georgyalex@gmail.com>
2023-10-09 21:56:00 +0400

    Изменен css

--- client/src/pages/LobbiPage/LobbiPage.tsx
+++ client/src/pages/LobbiPage/LobbiPage.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Button } from "../../components"
 import { useState } from "react";
-import { automat, RPG, general, tank2, tank3 } from "./images";
+import { automat, RPG, general, tank2, tank3 } from "./images"; 
 import cn from "classnames";
 import "./LobbiPage.css";
 
@@ -11,17 +11,17 @@ const LobbiPage: React.FC = () => {
 
     return(
         <div className="lobbi_block">
-            
+
             <div className="header">
                 <Button className="lobby" appearance="primary"> Лобби </Button>
                 <Button className="dosye" appearance="primary" onClick={() => {}}> Досье </Button>
                 <Button className="menu_button" appearance="primary" onClick={() => {}}> Меню </Button>
             </div>
-        
+
             <div className="mans">
                 <Button className="automat" appearance="image" onClick={() => {}}> Пехотинец-<br/>автоматчик <img src={automat} alt="Automat" /></Button>
                 <Button className="RPG"appearance="image"  onClick={() => {}}> Пехотинец с <br/>гранатомётом <img src={RPG} alt="RPG" /></Button>
-                
+
                 <Button className="general" appearance="image" onClick={() => {changeStatusGeneral()}}> Генерал {'('}
                     <span className={cn("status", {
                         ["free"]: !statusGeneral,
@@ -34,7 +34,7 @@ const LobbiPage: React.FC = () => {
                 <Button className="tank2" appearance="image" onClick={() => {}}> Двуместный танк <img src={tank2} alt="Tank_2" /> </Button>
                 <Button className="tank3" appearance="image" onClick={() => {}}> Трёхместный танк <img src={tank3} alt="Tank_3" /> </Button>
             </div>
-                
+
         </div>
     )
 }
