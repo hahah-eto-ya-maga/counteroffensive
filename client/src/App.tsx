c90fa0e995cba69a1313f080a337c9c23b4440d2
ester kinsky <esterkinsky@mail.ru>
2023-10-10 23:41:46 +0400

    fixed primary-disabled btns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./styles/global.css";
-import { LoginPage, SignInPage, MenuPage } from "./pages";
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <MenuPage />
     </div>
   );
 };
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,6 +1,6 @@
 import LoadingPage from "./LoadingPage/LoadingPage";
-import SignInPage from "./Auth/SigninPage/SigninPage";
+import SignInPage from "./Auth/SignInPage/SignInPage";
 import LoginPage from "./Auth/LoginPage/LoginPage";
-import MenuPage from './MenuPage/MenuPage';
+import MenuPage from "./MenuPage/MenuPage";
 
 export { LoadingPage, SignInPage, LoginPage, MenuPage };
