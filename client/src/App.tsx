8c747198d87ad87c6f71436d5b8724cd70a39aaa
Shi-Geo <shihaleev.georgyalex@gmail.com>
2023-10-11 09:13:26 +0400

    Кнопка (пехотинец автоматчик) - смена картинки

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,12 @@ import { useServer } from "./modules";
 import { ServerContext } from "./context";
 import { HOST } from "./config";
 import "./styles/global.css";
-import { LoginPage } from "./pages";
-import LobbiPage from "./pages/LobbiPage/LobbiPage";
 
 const App: React.FC = () => {
   const server = useServer(HOST);
   return (
     <ServerContext.Provider value={server}>
       <div>
-        <LobbiPage/>
       </div>
     </ServerContext.Provider>
   );
--- client/src/pages/GamePage/GamePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const GamePage: React.FC = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-} 
